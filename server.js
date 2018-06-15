933bbe8cd9a73c9d3b1f3980670e581ce1880e60
Ritika Gupta <ritika.emilence@gmail.com>
2018-06-15 17:02:16 +0530

    post api added

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-var PORT =process.env.PORT || 3000;
-var todos = [{
-	id : 1,
-	description:'Meet mom for lunch',
-	completed:false
-},
-{
-id : 2,
-	description:'Go to market',
-	completed:false
-	},
-	{
-id : 3,
-	description:'Feed the cat',
-	completed:true
-	}
-];
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
 app.get('/',function(req,res)
 {
 	res.send('Todo API root');
@@ -48,6 +38,18 @@ app.get('/todos/:id',function(req,res)
 	{
 		res.status(404).send();
 	}
+}
+);
+app.post('/todos',function(req,res)
+{
+	var body = req.body;
+
+	
+	
+	body.id = todoNextId;
+	todoNextId++;
+todos.push(body);
+	res.json(body);
 }
 );
 app.listen(PORT,function()
