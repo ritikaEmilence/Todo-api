d491dea95ef3ad47dab73efebb420c98ed584182
Ritika Gupta <ritika.emilence@gmail.com>
2018-06-21 12:40:31 +0530

    Delte odo added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require("underscore");
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -20,16 +21,8 @@ app.get('/todos',function(req,res)
 app.get('/todos/:id',function(req,res)
 {
 	var todoID = parseInt(req.params.id,10);
-	var matchedTodo;
-	todos.forEach(function(todo)
-	{
-		if (todo.id === todoID)
-		{
-			matchedTodo = todo;
-		}
-		
-	}
-	);
+	var matchedTodo = _.findWhere(todos,{id : todoID});
+	
 	if(matchedTodo)
 	{
 		res.json(matchedTodo);
@@ -40,10 +33,32 @@ app.get('/todos/:id',function(req,res)
 	}
 }
 );
+
+app.delete('/todos/:id',function(req,res)
+{
+	var todoID = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos,{id : todoID});
+	
+	if(!matchedTodo)
+	{ 
+		res.status(404).json({"error": "No todo item found on this index"});
+	}
+	else
+	{
+		todos = _.without(todos,matchedTodo);
+		res.json(todos);
+	}
+}
+);
 app.post('/todos',function(req,res)
 {
-	var body = req.body;
-
+var body = _.pick(req.body,'description','completed');
+if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0)
+{
+	return res.status(400).send();
+}
+	
+	body.description = body.description.trim();
 	
 	
 	body.id = todoNextId;
