fce096c83d6d609d8f5b55650c2f6e4efdf146f1
Ritika Gupta <ritika.emilence@gmail.com>
2018-06-21 13:19:33 +0530

    Update todo added

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
+
+//get
 app.get('/',function(req,res)
 {
 	res.send('Todo API root');
@@ -18,6 +20,7 @@ app.get('/todos',function(req,res)
 	res.json(todos);
 }
 );
+//get by id 
 app.get('/todos/:id',function(req,res)
 {
 	var todoID = parseInt(req.params.id,10);
@@ -33,7 +36,7 @@ app.get('/todos/:id',function(req,res)
 	}
 }
 );
-
+//delete
 app.delete('/todos/:id',function(req,res)
 {
 	var todoID = parseInt(req.params.id,10);
@@ -50,6 +53,7 @@ app.delete('/todos/:id',function(req,res)
 	}
 }
 );
+//post 
 app.post('/todos',function(req,res)
 {
 var body = _.pick(req.body,'description','completed');
@@ -67,6 +71,48 @@ todos.push(body);
 	res.json(body);
 }
 );
+//update
+app.put('/todos/:id',function(req,res)
+{
+	var body = _.pick(req.body,'description','completed');
+
+	var validAttributes = {};
+
+
+	var todoID = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos,{id : todoID});
+	
+	if(!matchedTodo)
+	{ 
+		return res.status(404).send();
+	}
+	
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed))
+{
+	validAttributes.completed = body.completed;
+}
+else if (body.hasOwnProperty('completed'))
+{
+	return res.status(400).send();
+}
+
+
+if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0)
+{
+	validAttributes.description = body.description;
+}
+else if (body.hasOwnProperty('description'))
+{
+	return res.status(400).send();
+}
+ _.extend(matchedTodo,validAttributes);
+res.json(matchedTodo);
+
+}
+);
+
+//listen server
 app.listen(PORT,function()
 	{
 		console.log('Express listening on port ' + PORT + '!');
